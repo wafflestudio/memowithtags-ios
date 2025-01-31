a4aec9770a1d2c84927706f39af93e852038c449
Swimming-in-Seoul <ryuswimming@snu.ac.kr>
2025-02-01 00:47:27 +0900

    deviceId 쓸 준비

--- MemoWithTags/MemoWithTags/Application/KeyChainManager.swift
+++ MemoWithTags/MemoWithTags/Application/KeyChainManager.swift
@@ -5,27 +5,22 @@
 //  Created by 최진모 on 1/3/25.
 //
 
-
 import Foundation
 import Security
 
+/// deviceId, accessToken, refreshtoken 관리
 final class KeyChainManager {
-    ///singleton
+    /// singleton
     static let shared = KeyChainManager()
     private init() {}
     
     private let service = "com.memoWithTags.service"
     
+    // MARK: - Access Token Methods
+    
     /// Keychain에 Access Token 저장, 이미 존재한다면 덮어씀
     func saveAccessToken(token: String) -> Bool {
         let account = "accessToken"
-        guard let data = token.data(using: .utf8) else { return false }
-        return save(service: service, account: account, data: data)
-    }
-    
-    /// Keychain에 Refresh Token 저장
-    func saveRefreshToken(token: String) -> Bool {
-        let account = "refreshToken"
         guard let data = token.data(using: .utf8) else { return false }
         return save(service: service, account: account, data: data)
     }
@@ -37,6 +32,21 @@ final class KeyChainManager {
         return String(data: data, encoding: .utf8)
     }
     
+    /// Keychain에서 Access Token 삭제하기
+    func deleteAccessToken() -> Bool {
+        let account = "accessToken"
+        return delete(service: service, account: account)
+    }
+    
+    // MARK: - Refresh Token Methods
+    
+    /// Keychain에 Refresh Token 저장
+    func saveRefreshToken(token: String) -> Bool {
+        let account = "refreshToken"
+        guard let data = token.data(using: .utf8) else { return false }
+        return save(service: service, account: account, data: data)
+    }
+    
     /// Keychain에서 Refresh Token 불러오기
     func readRefreshToken() -> String? {
         let account = "refreshToken"
@@ -44,17 +54,38 @@ final class KeyChainManager {
         return String(data: data, encoding: .utf8)
     }
     
-    /// Keychain에서 Access Token 삭제하기
-    func deleteAccessToken() -> Bool {
-        let account = "accessToken"
-        return delete(service: service, account: account)
-    }
-    
     /// Keychain에서 Refresh Token 삭제하기
     func deleteRefreshToken() -> Bool {
         let account = "refreshToken"
         return delete(service: service, account: account)
     }
+    
+    // MARK: - Device ID Methods
+    
+    /// Keychain에 Device ID 저장, 이미 존재한다면 반환
+    var deviceId: String {
+        if let id = readDeviceId() {
+            return id
+        } else {
+            let newID = UUID().uuidString
+            let success = saveDeviceId(id: newID)
+            return success ? newID : UUID().uuidString // Fallback in case of failure
+        }
+    }
+    
+    private func saveDeviceId(id: String) -> Bool {
+        let account = "deviceID"
+        guard let data = id.data(using: .utf8) else { return false }
+        return save(service: service, account: account, data: data)
+    }
+    
+    private func readDeviceId() -> String? {
+        let account = "deviceID"
+        guard let data = read(service: service, account: account) else { return nil }
+        return String(data: data, encoding: .utf8)
+    }
+    
+    // MARK: - Generic Keychain Methods
     
     /// Keychain에 데이터 저장
     private func save(service: String, account: String, data: Data) -> Bool {
@@ -81,7 +112,7 @@ final class KeyChainManager {
     }
     
     /// Keychain에서 데이터 불러오기
-    func read(service: String, account: String) -> Data? {
+    private func read(service: String, account: String) -> Data? {
         let query = [
             kSecClass as String: kSecClassGenericPassword,
             kSecAttrService as String: service,
@@ -100,7 +131,7 @@ final class KeyChainManager {
     }
     
     /// Keychain에서 데이터 삭제
-    func delete(service: String, account: String) -> Bool {
+    private func delete(service: String, account: String) -> Bool {
         let query = [
             kSecClass as String: kSecClassGenericPassword,
             kSecAttrService as String: service,
@@ -111,3 +142,4 @@ final class KeyChainManager {
         return status == errSecSuccess
     }
 }
+
