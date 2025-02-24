905dd4427061d64912cd98c9e2a49969a2e45395
Swimming-in-Seoul <ryuswimming@snu.ac.kr>
2025-02-25 01:27:25 +0800

    MemoListView 살짝 만짐

--- MemoWithTags/MemoWithTags/Presentation/View/SubView/MemoListView.swift
+++ MemoWithTags/MemoWithTags/Presentation/View/SubView/MemoListView.swift
@@ -5,21 +5,14 @@ struct MemoListView: View {
     
     var body: some View {
         ScrollView {
-            
             LazyVStack(alignment: .leading, spacing: 12) {
                 ForEach(viewModel.memos.reversed()) { memo in
-                    if #available(iOS 18.0, *) {
-                        MemoView(memo: memo, viewModel: viewModel)
-                            .id(memo.id)
-                    } else {
-                        // 애니메이션이 일단 ios18만 지원되는 상태..
-                    }
+                    MemoView(memo: memo, viewModel: viewModel)
+                        .id(memo.id)
                 }
             }
             .padding(.bottom, 20)
-
         }
         .defaultScrollAnchor(.bottom)
-        
     }
 }
